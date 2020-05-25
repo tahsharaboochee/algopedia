72eeb781784a15f797d0d6994a1a0d276e50063b
Tahshara Boochee <boocheet@gmail.com>
2020-05-25 13:29:16 -0700

    removed build from git ignore to deploy on netlify

---
+++ build/precache-manifest.81c1c5a599450a60f5ea213f753aa17d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18fa6a78aeafede123d1610a3234131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af54a1673571d2673ec5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0c469bf9abcced68781e",
+    "url": "/static/css/main.106cb5b7.chunk.css"
+  },
+  {
+    "revision": "af54a1673571d2673ec5",
+    "url": "/static/js/2.62361c1c.chunk.js"
+  },
+  {
+    "revision": "d287490df2068f4431dbac0aaabe190c",
+    "url": "/static/js/2.62361c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c469bf9abcced68781e",
+    "url": "/static/js/main.b2dd0d9b.chunk.js"
+  },
+  {
+    "revision": "4577d7dbe2d2abf7c07f",
+    "url": "/static/js/runtime-main.67ca31fd.js"
+  }
+]);
